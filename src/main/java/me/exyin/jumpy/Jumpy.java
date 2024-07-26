725f8f9d26b1e60fa181ddf4a4328789af40f130
Jacques Tea <jacquestea16@gmail.com>
2024-07-26 14:19:36 +0200

    Initial commit

---
+++ src/main/java/me/exyin/jumpy/Jumpy.java
@@ -0,0 +1,17 @@
+package me.exyin.jumpy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Jumpy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
