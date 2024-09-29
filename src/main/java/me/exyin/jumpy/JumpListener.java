0bb768398eb183a00cd7fb65001a885d972534f3
Jacques Tea <jacquestea16@gmail.com>
2024-09-29 11:14:34 +0200

    Changed import

--- src/main/java/me/exyin/jumpy/JumpListener.java
+++ src/main/java/me/exyin/jumpy/JumpListener.java
@@ -5,10 +5,7 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerGameModeChangeEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.event.player.*;
 
 public class JumpListener implements Listener {
     private final Jumpy jumpy;
